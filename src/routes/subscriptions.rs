023de09a261e79697794a92ec8d684635938eeab
Noam Cohen <cnoam@technion.ac.il>
2023-10-22 12:55:54 +0300

    code in chapter 4.5.12
    use tracing::instrument as a macro

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,8 +2,6 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use uuid::Uuid;
 use sqlx::types::chrono::Utc;
-use tracing::span::Entered;
-use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,37 +13,34 @@ pub struct FormData {
 // curl -X POST localhost:8000/subscriptions    -H "Content-Type: application/x-www-form-urlencoded"  --data "name=noam&email=g@g.com"
 
 // check the tracing docs: https://docs.rs/tracing/latest/tracing/
-
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        request_id = % Uuid::new_v4(),
+        subscriber_email = % form.email, subscriber_name = % form.name
+    )
+)]
 pub(crate) async fn subscribe(form: web::Form<FormData>,
                               pool: web::Data<PgPool>, ) -> HttpResponse {
-    /* 3.9.3
-    web::Data, when a new request comes in, computes the TypeId of the type you specified in the signature (in
-our case PgConnection) and checks if there is a record corresponding to it in the type-map. If there is one, it
-casts the retrieved Any value to the type you specified (TypeId is unique, nothing to worry about) and passes
-it to your handler.
-It is an interesting technique to perform what in other language ecosystems might be referred to as depend-
-ency injection.
-     */
 
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-        );
-    // Using `enter` in an async function is a recipe for disaster!
-    // Bear with me for now, but don't do this at home.
-    // See the following section on `Instrumenting Futures`
-    let _request_span_guard : Entered= request_span.enter();
+    match insert_subscriber(&pool, &form).await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
 
-    // We do not call `.enter` on query_span!
-    // `.instrument` takes care of it at the right moments
-    // in the query future lifetime
-    let query_span = tracing::info_span!("Saving new subscriber details in the database");
-    //tracing::info!("request_id {} - Saving new subscriber details in the database",request_id);
 
-    let result = sqlx::query!(
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    form: &FormData,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -55,19 +50,11 @@ ency injection.
         form.name,
         Utc::now()
     )
-    // We use `get_ref` to get an immutable reference to the `PgConnection`
-    // wrapped by `web::Data`.
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await;
-
-    match result{
-        Ok(_) => {
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!("request_id {} Failed to execute query: {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
-    }
+        .execute(pool)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+        })?;
+        Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
 //! tests/health_check.rs
 use std::net::TcpListener;
 
-use sqlx::{Connection, Executor, PgConnection};
-use sqlx::PgPool;
+use sqlx::{Connection, Executor, PgConnection, PgPool};
 use sqlx::types::Uuid;
 
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+use once_cell::sync::Lazy;
+
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
@@ -32,6 +34,30 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    // We cannot assign the output of `get_subscriber` to a variable based on the
+    // value TEST_LOG` because the sink is part of the type returned by
+    // `get_subscriber`, therefore they are not the same type. We could work around
+    // it, but this is the most straight-forward way of moving forward.
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::stdout,
+        );
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(
+            subscriber_name,
+            default_filter_level,
+            std::io::sink,
+        );
+        init_subscriber(subscriber);
+    };
+});
 
 // added in chapter 3.10
 pub struct TestApp {
@@ -39,8 +65,12 @@ pub struct TestApp {
     pub db_pool: PgPool,
 }
 
-// the func is now async
+
 async fn spawn_app() -> TestApp {
+    // The first time `initialize` is invoked the code in `TRACING` is executed.
+    // All other invocations will instead skip execution.
+    Lazy::force(&TRACING);
+
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
