dd629bd05d9f15149ea9f42cc7ce21114a92d7dc
Noam Cohen <cnoam@technion.ac.il>
2023-11-20 15:33:42 +0200

    code for 7.7.5.3
    refactor
    all tests PASS

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -3,13 +3,11 @@
 use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
+use wiremock::MockServer;
 
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::{Application, get_connection_pool};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use wiremock::MockServer;
-
-// before refactoring it was use sqlx::types::Uuid;
 
 // Ensure that the `tracing` stack is only initialised once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -44,6 +42,12 @@ pub struct TestApp {
     pub port: u16,
 }
 
+/// Confirmation links embedded in the request to the email API.
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
+}
+
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
@@ -53,6 +57,36 @@ impl TestApp {
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+
+    /// Extract the confirmation links embedded in the request to the email API.
+    pub fn get_confirmation_links(&self, email_request: &wiremock::Request)
+                                  -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(
+            &email_request.body
+        ).unwrap();
+        // Extract the link from one of the request fields.
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            // Let's make sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+
+        let html = get_link(&body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(&body["TextBody"].as_str().unwrap());
+        ConfirmationLinks {
+            html,
+            plain_text,
+        }
     }
 }
 
@@ -86,7 +120,7 @@ pub async fn spawn_app() -> TestApp {
         address: format!("http://localhost:{}", application_port),
         port: application_port,
         db_pool: get_connection_pool(&configuration.database),
-        email_server
+        email_server,
     }
 }
 
--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -8,7 +8,7 @@ use crate::helpers::spawn_app;
 
 #[tokio::test]
 async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
-// Arrange
+    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     Mock::given(path("/email"))
@@ -16,27 +16,13 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
+
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body)
-        .unwrap();
-    // Extract the link from one of the request fields.
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
-        assert_eq!(links.len(), 1);
-        links[0].as_str().to_owned()
-    };
-    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
-    // Let's make sure we don't call random APIs on the web
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-    // Let's rewrite the URL to include the port
-    confirmation_link.set_port(Some(app.port)).unwrap();
+    let confirmation_links = app.get_confirmation_links(&email_request);
+
     // Act
-    let response = reqwest::get(confirmation_link)
+    let response = reqwest::get(confirmation_links.html)
         .await
         .unwrap();
     // Assert
