de27defc4504f9a95968978e2e550e2a5476fd2c
Noam Cohen <cnoam@technion.ac.il>
2024-01-07 17:07:22 +0200

    code for 10.7.4
    Add Redis to store session data for each user.

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,6 +14,8 @@ pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
     pub email_client: EmailClientSettings,
+    // The URI is marked as secret because it may embed a password.
+    pub redis_uri: Secret<String>,
 }
 
 
--- src/startup.rs
+++ src/startup.rs
@@ -15,6 +15,8 @@ use actix_web_flash_messages::FlashMessagesFramework;
 use actix_web_flash_messages::storage::CookieMessageStore;
 use secrecy::ExposeSecret;
 use actix_web::cookie::Key;
+use actix_session:: SessionMiddleware;
+use actix_session::storage::RedisSessionStore;
 
 // We need to define a wrapper type in order to retrieve the URL
 // in the `subscribe` handler.
@@ -22,23 +24,32 @@ use actix_web::cookie::Key;
 // a raw `String` would expose us to conflicts.
 pub struct ApplicationBaseUrl(pub String);
 
-fn run(
+async fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
     hmac_secret: Secret<String>,
-) -> Result<Server, std::io::Error> {
+    redis_uri: Secret<String>,
+) -> Result<Server, anyhow::Error> {
+
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
     let base_url = Data::new(ApplicationBaseUrl(base_url));
     // Capture `connection` from the surrounding environment ---> add "move"
-    let message_store = CookieMessageStore::builder(Key::from(hmac_secret.expose_secret().as_bytes())).build();
+    let redis_store_res = RedisSessionStore::new(redis_uri.expose_secret()).await;
+    if redis_store_res.is_err(){
+        return Err(anyhow::anyhow!("Failed connecting to Redis"));
+    }
+    let redis_store  = redis_store_res.unwrap();
+    let secret_key = Key::from(hmac_secret.expose_secret().as_bytes());
+    let message_store = CookieMessageStore::builder(secret_key.clone()).build();
     let message_framework = FlashMessagesFramework::builder(message_store).build();
     let server = HttpServer::new(move || {
         App::new()
             // Middlewares are added using the `wrap` method on `App`
-            .wrap(message_framework.clone()) // noam: why need clone()
+            .wrap(message_framework.clone())
+            .wrap( SessionMiddleware:: new(redis_store.clone(), secret_key.clone()))
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
@@ -72,7 +83,7 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .connect_lazy_with(configuration.with_db())
 }
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
+pub async fn build(configuration: Settings) -> Result<Server, anyhow::Error> {
     let connection_pool = get_connection_pool(&configuration.database);
 
     // Build an `EmailClient` using `configuration`
@@ -91,7 +102,7 @@ pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
     let address = format!("{}:{}",
                           configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool, email_client, configuration.application.base_url, configuration.application.hmac_secret,)
+    run(listener, connection_pool, email_client, configuration.application.base_url, configuration.application.hmac_secret, configuration.redis_uri).await
 }
 
 // hack to get the server port number.
@@ -102,7 +113,7 @@ pub struct Application {
 }
 
 impl Application {
-    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+    pub async fn build(configuration: Settings) -> Result<Self, anyhow::Error> {
         let connection_pool = get_connection_pool(&configuration.database); // in branch 10.a there is await()???
         let sender_email = configuration
             .email_client
@@ -125,9 +136,10 @@ impl Application {
             listener,
             connection_pool,
             email_client,
-                         configuration.application.base_url,
-                         configuration.application.hmac_secret
-        )?;
+            configuration.application.base_url,
+            configuration.application.hmac_secret,
+            configuration.redis_uri
+        ).await?;
 
         Ok(Self { port, server })
     }
